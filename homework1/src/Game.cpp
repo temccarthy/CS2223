091177cfbb96a38fc91b5112bfb77bf54ff55ad0
Tim McCarthy <timothy.mcc@comcast.net>
2019-10-26 19:20:28 -0400

    finished game

--- homework1/src/Game.cpp
+++ homework1/src/Game.cpp
@@ -8,13 +8,15 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <stdbool>
+#include <stdbool.h>
 using namespace std;
 #include "Game.h"
 
 
 Game::Game(){
-	colors = {3,5,7};
+	colors[0] = 3;
+	colors[1] = 5;
+	colors[2] = 7;
 }
 
 Game::~Game(){
@@ -28,27 +30,35 @@ void Game::makeRandomMove(){
 	srand(time(NULL));
 	int index = -1;
 	bool done = true;
+
 	while (!done){
 		index = rand()%3;
 		if (colors[index] != 0){
 			int numLeft = colors[index];
-			int numTake =
+			int numTake = rand()%(numLeft-1)+1;
 			makeMove(index, numTake);
 			done = true;
 		}
 	}
-
-
-
 }
 
 void Game::makeComputerMove(){
 	int sum = findNimSum();
-	int index;
+	int index=-1;
 	if (sum==0){
 		index = -1;
 	} else {
+		for (int i = 0; i<3; i++){
+			if (sum^colors[i == sum]){
+				index = i;
+			}
+		}
+	}
 
+	if (index == -1){
+		makeRandomMove();
+	} else {
+		makeMove(index, sum);
 	}
 	/*
 	 *int sum = finsnimsum()
@@ -77,5 +87,5 @@ void Game::makeComputerMove(){
 }
 
 int Game::findNimSum(){
-	return orange^yellow^green;
+	return colors[0]^colors[1]^colors[2];
 }
