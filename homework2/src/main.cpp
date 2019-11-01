3896dbcdcbe809b66cd313402038d3e26ec3998a
Tim <timothy.mcc@comcast.net>
2019-11-01 16:26:21 -0400

    restructuring

--- homework2/src/main.cpp
+++ homework2/src/main.cpp
@@ -7,16 +7,29 @@
  * R(n) = 4R(n−1)−3R(n−2)
  * R(0) = 2
  * R(1) = 8
+ * R(2) = 26
+ * R(3) = 80
+ * R(4) = 242
  *
- *
- *
- * R(n)
- *
- *
- *
+ * R(n) = 3^n-1
  *
  */
 
+#include <iostream>
+using namespace std;
+#include "main.h"
 
+int main(int argc, char* argv[]){
+	cout << Lucas(25);
 
+	return 0;
+}
 
+int Lucas(int n){
+	if (n==0) {
+		return 2;
+	} else if (n==1) {
+		return 1;
+	}
+	return Lucas(n-1) + Lucas(n-2);
+}
---
+++ homework2/src/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Nov 1, 2019
+ *      Author: Tim
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int Lucas(int n);
+
+
+
+#endif /* MAIN_H_ */
