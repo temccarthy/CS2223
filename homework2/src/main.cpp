fa453f27c518454edaa26fe26926868aae54c8ca
Tim <timothy.mcc@comcast.net>
2019-11-01 15:55:07 -0400

    homework 2 added

---
+++ homework2/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 1, 2019
+ *      Author: Tim McCarthy and Jarius Thomas
+ * Question 1:
+ * R(n) = 4R(n−1)−3R(n−2)
+ * R(0) = 2
+ * R(1) = 8
+ *
+ *
+ *
+ * R(n)
+ *
+ *
+ *
+ *
+ */
+
+
+
+
